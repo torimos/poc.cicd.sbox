14cd4a471d7571604331e61a676a2991a8cc319e
Oleg Lytvynenko <Oleg_Lytvynenko@epam.com>
2017-03-15 17:38:51 +0200

    fix

--- make.sh
+++ make.sh
@@ -1,6 +1,9 @@
 mkdir ncore
 cd ncore
 dotnet new sln -n ncore
-cd app
 dotnet new console -n app -f netcoreapp1.1
 dotnet sln ncore.sln add app/app.csproj
+dotnet restore
+dotnet build
+dotnet run --project app/app.csproj
+cd ..
