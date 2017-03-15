4b227443b9d8d9e94a5bca828186aec1571aecff
Oleg Lytvynenko <Oleg_Lytvynenko@epam.com>
2017-03-15 19:15:00 +0200

    added web

--- make.bat
+++ make.bat
@@ -2,8 +2,9 @@ mkdir ncore
 cd ncore
 dotnet new sln -n ncore
 dotnet new console -n app -f netcoreapp1.1
+dotnet new mvc -n web -f netcoreapp1.1
 dotnet sln ncore.sln add app/app.csproj
+dotnet sln ncore.sln add web/web.csproj
 dotnet restore
 dotnet build
-dotnet run --project app/app.csproj
 cd ..
